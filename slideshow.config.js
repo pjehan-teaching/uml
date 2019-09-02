8bb2160f01c18bee026979693e64d745f54422db
pjehan <pierre.jehan@gmail.com>
2019-09-03 01:31:37 +0200

    Add slides to presentation, use case, activity and sequence chapters

--- slideshow.config.js
+++ slideshow.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   name: 'UML',
-  stylesheets: ['assets/css/style.css']
+  stylesheets: ['assets/chartjs/Chart.min.css', 'assets/css/style.css'],
+  javascripts: ['assets/chartjs/Chart.bundle.min.js', 'assets/js/script.js']
 }
