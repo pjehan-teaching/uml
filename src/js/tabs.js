f788c4cbddff92a7abf1ec36498e963a78df7c6a
pjehan <pierre.jehan@gmail.com>
2023-03-17 15:08:49 +0100

    Improve tabs JS and CSS code to manage multiple tabs container

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -6,14 +6,27 @@ document.getElementById('revealexpress').addEventListener('loaded', () => {
     button.addEventListener('click', e => {
       e.preventDefault();
 
-      const container = e.currentTarget.closest('.tabs-container');
-      const targetClass = e.currentTarget.dataset.target;
-      const tabs = container.querySelectorAll('.tabs > *');
+      const targetSelector = e.currentTarget.dataset.target;
+      const targetEverywhere = e.currentTarget.dataset.targetEverywhere;
+      let container = targetEverywhere ? document.querySelector('#revealexpress') : e.currentTarget.closest('.tabs-container');
 
-      for (const tab of tabs) {
-        tab.style.display = 'none';
-      }
-      container.querySelector('.' + targetClass).style.display = 'block';
+      // Activate the button(s)
+      Array.from(container.querySelectorAll('.tab-button')).forEach(btn => {
+        // Check if the current button is in a group containing a button with the current target selector
+        if (btn.closest('.tabs-buttons-container').querySelectorAll('[data-target="' + targetSelector + '"]').length > 0) {
+          if (btn.dataset.target === targetSelector) {
+            btn.classList.add('active');
+          } else {
+            btn.classList.remove('active');
+          }
+        }
+      });
+
+      // Hide all tabs
+      Array.from(container.querySelectorAll('.tabs > *')).forEach(tab => tab.classList.remove('active'));
+
+      // Show the target tab(s)
+      Array.from(container.querySelectorAll(targetSelector)).forEach(tab => tab.classList.add('active'));
     });
   }
 
