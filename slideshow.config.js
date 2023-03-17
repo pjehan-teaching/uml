e78e25dcefab9b5299126e40053a287989b9938d
pjehan <pierre.jehan@gmail.com>
2023-03-17 15:06:56 +0100

    Remove unused ChartJS library files from loading

--- slideshow.config.js
+++ slideshow.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   name: 'UML',
-  stylesheets: ['assets/chartjs/Chart.min.css', 'assets/css/style.css'],
-  javascripts: ['assets/chartjs/Chart.bundle.min.js', 'assets/js/script.js']
+  stylesheets: ['assets/css/style.css'],
+  javascripts: ['assets/js/script.js']
 }
